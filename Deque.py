7ae782691568b2915296b039f1735d33dd04afdb
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2020-07-24 23:02:53 +0000

    Update Deque.py

--- Deque.py
+++ Deque.py
@@ -5,23 +5,27 @@
 import random
 class Deque:
 
-    MaxArraySize =20
+    MaxArraySize =20  # default array size
     def __init__(self):
         self.items=[0]*Deque.MaxArraySize
         self.size=0
-        self.front=-1
-        self.rear=-1
+        self.front=-1  #front of the array
+        self.rear=-1   #back of the arrray
 
 
+#Funtion to check if the array is full
     def isFull(self):
-
+        #Instances for when there is no more space in the array
         if (self.front == 0 and self.rear ==len(self.items)-1) or (self.front == self.rear+1):
            return True
 
         else:
+        #returns false when there is space
             return False
 
+#Function to check if the array is empty
     def isEmpty(self):
+        #when they all still point to their intial initialization returns the empty array
         if (self.front==-1 and self.rear == -1):
             return True
 
@@ -30,33 +34,33 @@ class Deque:
             
 
     def Insertfront(self,data):
-
-        if(self.isFull()):
+        if(self.isFull()): #Calls the function
             print("The Array is maxed out")
             print("Cannot insert %d"%data)
         else:
             if(self.isEmpty()):
-                self.front = 0
+                self.front = 0 #Points to the first index
                 self.rear = 0
             elif(self.front == 0):
-                self.front = len(self.items)-1
+                self.front = len(self.items)-1 #When inputting, next index will begin from the end of the array
             else:
-                self.front-=1
-        self.items[self.front]=data
+                self.front-=1 #Subtracts from the index
+        self.items[self.front]=data #Assigns the data into the array
+
 
     def Insertrear(self,data):
         if(self.isFull()):
             print("The Array is maxed out")
             print("Cannot insert %d"%data)
         else:
-            if(self.isEmpty()):
-                self.front =0
+            if(self.isEmpty()): #Calls the function
+                self.front =0 #Points to the first index
                 self.rear=0
-            elif(self.rear==len(self.items)-1):
+            elif(self.rear==len(self.items)-1): #Starts inputting from the last index of the array
                 self.rear=0
             else:
-                self.rear+=1
-        self.items[self.rear]=data
+                self.rear+=1 #Adds 1 to index
+        self.items[self.rear]=data #Assigns the data into the array
 
     
     def Removefront(self):
@@ -92,13 +96,15 @@ class Deque:
         i = self.front
         print("Current Size:")
         while(i!=self.rear):
-            print("%d" %self.items[i],end=",")
-            i=(i+1)%len(self.items)
-        print("%d" %self.items[self.rear])
+            print("%d" % self.items[i],end=",")
+            i=(i+1)% len(self.items)
+        print("%d" % self.items[self.rear])
 
+    #Function that return the current front element
     def getFront(self):
         print("The Current Front element is: %d " %self.items[self.front])
-
+    
+    #Function that return the current front element
     def getRear(self):
         print("The Current Rear element is: %d " %self.items[self.rear])
 
@@ -122,7 +128,7 @@ class Deque:
 ##            print("Error")
 ##        self.display()
         
-
+#Creates an Object
 Deck = Deque()
 Deck.Insertfront(2)
 Deck.Insertfront(3)
